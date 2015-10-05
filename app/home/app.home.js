ad50bca1dd0f147f4217695df5ac4973cce1e316
jeins <mail@mjuanakbar.info>
2015-10-05 12:53:42 +0200

    add home namespaces

---
+++ app/home/app.home.js
@@ -0,0 +1,7 @@
+define(['angular'], function (angular) {
+    'use strict';
+
+    var home = angular.module('app.home', []);
+
+    return home;
+});
---
+++ app/home/app.home.req.js
@@ -0,0 +1,3 @@
+define([
+    '/app/home/controllers/CtrlHome',
+], function(){});
